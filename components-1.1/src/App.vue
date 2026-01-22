4cec0e65d93fab7de3556ea7f224bcb61c00446e
Alenga <a24alencf@iessanclemente.net>
2026-01-22 11:27:48 +0100

    jan 22

---
+++ components-1.1/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+            message: 'ola mundo',
+            count: 0,
+        };
+    },
+    methods: {
+        toggleCaps() {
+            this.message =
+                this.message[0] === 'o' ? this.message.toUpperCase() : this.message.toLowerCase();
+        },
+    },
+};
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+    <button @click="toggleCaps">Toggle caps</button>
+</template>
+
+<style scoped></style>
